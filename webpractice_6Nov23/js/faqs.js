a6fb48f2beb4c0b3c088e63d24445e4435e29c2a
Sometri OENG <67381317+sometri@users.noreply.github.com>
2023-11-12 21:02:09 +0700

    Delete webpractice_6Nov23 directory

--- webpractice_6Nov23/js/faqs.js
+++
@@ -1,8 +0,0 @@
-function toggleAnswer(id) {
-  var answer = document.getElementById(id);
-  if (answer.style.display === "none") {
-    answer.style.display = "block";
-  } else {
-    answer.style.display = "none";
-  }
-}
